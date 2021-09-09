acfc4993f2c9084d1cb713b7d025c708f7b85389
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-09 16:34:02 +0100

    whiped contents to begin actual task

--- app.rb
+++ app.rb
@@ -1,25 +1,4 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/' do
-  "home"
-end
-
-get '/secret' do
-  "Hello again"
-end
-
-get '/random-cat' do
- @name = ["Amigo", "Mi sty", "Almond"].sample
-erb(:index)
-end
-
-get '/cat-form' do
-  erb(:cat_form)
-end
-
-post '/named-cat' do
-  @name = params[:name]
-  erb(:index)
-end
 
