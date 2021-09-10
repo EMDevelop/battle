54143896f0dec58736e72a3ca66c926b1557182a
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-10 10:57:43 +0100

    Refactored code with a /redirect, enabling secrets and using the hash within another get route to display the code

--- app.rb
+++ app.rb
@@ -2,14 +2,22 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 class Battle < Sinatra::Base
+  
+  set :session_secret, 'super secret'
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
+  get '/play' do
+    @player_one, @player_two = session[:player_one], session[:player_two]
+    erb :play
+  end
+
   post '/names' do
-    @player_one, @player_two = params[:player_one], params[:player_two]
-    erb :play  
+    session[:player_one], session[:player_two] = params[:player_one], params[:player_two]
+    redirect '/play'
   end
 
 end
--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -2,13 +2,10 @@ describe 'the home screen', type: :feature do
 
     it 'checks names appear on screen from a form submit' do
       visit('/')
-
       fill_in('player_one', with: 'Ed')
       fill_in('player_two', with: 'Billy')
       click_button('Play')
-
-      expect(page).to have_content 'Ed'
-      expect(page).to have_content 'Billy'
+      expect(page).to have_content 'Player One: Ed, Player Two: Billy'
     end
 
 end
