17a24fa1d4fda3e732a74f4809aee77c13bb583a
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-10 14:03:04 +0100

    added player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,16 @@
+require 'player'
+
+describe Person do
+
+  let(:ed) { Person.new "Ed" }
+
+  context '#name' do
+    
+
+    it 'returns instance name' do
+      expect(ed.name).to eq 'Ed'
+    end
+
+  end
+
+end
